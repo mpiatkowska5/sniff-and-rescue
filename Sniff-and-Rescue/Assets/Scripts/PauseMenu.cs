e555d3898af04a0f333f32dbd3152433b55adb3f
Mattuhh <matias.haakana@outlook.com>
2026-03-30 23:00:14 +0300

    Added new main menu + splashscreen + vfx
    Also added snow particles, tested out water shader and added a pause menu

---
+++ Sniff-and-Rescue/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+    
+    [SerializeField] Button settingsBtn;
+    [SerializeField] Button continueBtn;
+
+    
+
+    public void Start()
+    {
+       pauseMenu.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused=false;
+    }
+
+   
+    
+}
---
+++ Sniff-and-Rescue/Assets/Scripts/SceneController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    
+    public void ChangeScene(string sceneName)
+    {
+        
+        SceneManager.LoadScene(sceneName);
+    }
+
+    
+}
