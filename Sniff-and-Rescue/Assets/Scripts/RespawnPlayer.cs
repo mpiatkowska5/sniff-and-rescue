02c417178c90e27ab3d858b8dbf1e06c784b2ab5
mpiatkowska5 <maja.piatkowska2004@gmail.com>
2026-04-20 17:52:10 +0300

    Added falling ice mechanic

---
+++ Sniff-and-Rescue/Assets/Scripts/RespawnPlayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+    [SerializeField] public Transform respawnPoint;
+    [SerializeField] GameObject playerChar;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player in water");
+            if (collider.TryGetComponent<PlayerController>(out PlayerController player))
+            {
+                player.SetParent(null);
+            }
+        }
+    }
+}
