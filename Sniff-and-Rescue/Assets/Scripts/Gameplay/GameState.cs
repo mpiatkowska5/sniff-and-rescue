98b16b0b88769a6855768e89ffa05f5d0fa855ca
mpiatkowska5 <maja.piatkowska2004@gmail.com>
2026-05-07 18:25:17 +0300

    Revert "Imported quiz package"
    This reverts commit 5fd2b3645f68fe358aea5b31c61ecf53287dc231.

--- Sniff-and-Rescue/Assets/Scripts/Gameplay/GameState.cs
+++
@@ -1,7 +0,0 @@
-public enum GameState
-{
-    Running,
-    QuizActive,
-    Paused,
-    GameEnded
-}
--- Sniff-and-Rescue/Assets/Scripts/medkit.cs
+++ Sniff-and-Rescue/Assets/Scripts/medkit.cs
@@ -5,33 +5,11 @@ public class medkit : MonoBehaviour, IInteractable
 	public static int playerScore;
 	public static int medKitsCollected = 0;
 
-    [SerializeField] QuestionData question;
-    [SerializeField] QuizManager quizManager;
-
-    private void Awake()
-    {
-        if (quizManager == null)
-        {
-            quizManager = FindFirstObjectByType<QuizManager>();
-        }
-    }
-
-    public void Interact()
+	public void Interact()
 	{
 		Debug.Log("interacted with medkit");
 		playerScore += 100;
 		medKitsCollected++;
-        if (quizManager == null)
-        {
-            InGameUIManager uiManager = FindFirstObjectByType<InGameUIManager>();
-            quizManager = uiManager != null ? uiManager.gameObject.AddComponent<QuizManager>() : null;
-        }
-
-        if (quizManager != null)
-        {
-            quizManager.StartQuiz(question);
-            Destroy(gameObject);
-        }
-        //Destroy(this.gameObject);
+		Destroy(this.gameObject);
 	}
 }
